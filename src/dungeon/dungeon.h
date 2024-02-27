eee5a45ca772be633c2cda4e9a544a06a786076d
d0gied <german.n.u@mail.ru>
2024-02-27 16:49:52 +0300

    Add IDungeonMaker class

--- src/dungeon/dungeon.h
+++ src/dungeon/dungeon.h
@@ -3,22 +3,32 @@
 #include <vector>
 
 namespace dungeon {
+class Dungeon;
+class IDungeonMaker;
+
+class IDungeonMaker {
+public:
+    virtual void build() = 0;
+    virtual Dungeon *getDungeon() = 0;
+};
+
 class Dungeon {
-  public:
-  Dungeon();
+public:
+    std::vector<Room *> getRooms();
+    Cell *getCurrentCell();
 
-  std::vector<Room *> getRooms();
-  Cell *getCurrentCell();
+    // find rooms which are connected with given room by corridor(cells)
+    std::vector<Room *> getRoomNeighbours(Room *room);
 
-  // find rooms which are connected with given room by corridor(cells)
-  std::vector<Room *> getRoomNeighbours(Room *room);
+    friend class IDungeonMaker;
 
-  private:
-  void generate(int seed);
+private:
+    // dungeon may be built only by DungeonMaker
+    Dungeon() = default;
 
-  std::vector<Room *> m_rooms;
-  std::vector<Cell *> m_cells;
+    std::vector<Room *> m_rooms;
+    std::vector<Cell *> m_cells;
 
-  Cell *m_current_cell = nullptr;
+    Cell *m_current_cell = nullptr;
 };
 } // namespace dungeon
