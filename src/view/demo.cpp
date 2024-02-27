4e0c0020360f3dc2285ec27c8fe42b8211299e0d
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-02-27 16:31:50 +0500

    CMakeLists compiler options fix

--- src/view/demo.cpp
+++ src/view/demo.cpp
@@ -4,15 +4,12 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    auto window = sf::RenderWindow{ { 920u, 1080u }, "CMake SFML Project" };
     window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+    while (window.isOpen()) {
+        for (auto event = sf::Event{}; window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
