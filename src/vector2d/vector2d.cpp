a2723b011981688b074b1a10d31ff2d5acdd5f77
sskotezhov <sskotezhov@edu.hse.ru>
2024-02-29 13:58:50 +0300

    const funcs

--- src/vector2d/vector2d.cpp
+++ src/vector2d/vector2d.cpp
@@ -2,11 +2,11 @@
 #include <cmath>
 using namespace std;
 Vector2d::Vector2d(double x, double y) : x(x), y(y){};
-Vector2d Vector2d::operator+(Vector2d b) {
-    return Vector2d(x + b.x, y + b.y);
+Vector2d Vector2d::operator+(const Vector2d &other) {
+    return Vector2d(x + other.x, y + other.y);
 }
-Vector2d Vector2d::operator-(Vector2d b) {
-    return Vector2d(x - b.x, y - b.y);
+Vector2d Vector2d::operator-(const Vector2d &other) {
+    return Vector2d(x - other.x, y - other.y);
 }
 Vector2d Vector2d::operator-() {
     return Vector2d(-x, -y);
@@ -20,15 +20,15 @@ Vector2d Vector2d::operator/(double b) {
 Vector2d Vector2d::round() {
     return Vector2d(roundf(x), roundf(y));
 }
-double Vector2d::lenght_squared() {
+double Vector2d::length_squared() {
     return pow(x, 2) + pow(y, 2);
 }
-double Vector2d::lenght() {
-    return sqrt(lenght_squared());
+double Vector2d::length() {
+    return sqrt(length_squared());
 }
 Vector2d Vector2d::normalized() {
-    return (*this) / lenght();
+    return (*this) / length();
 }
-int Vector2d::manhattan_lenght() {
+int Vector2d::manhattan_length() {
     return std::round(fabs(x) + fabs(y));
 }
