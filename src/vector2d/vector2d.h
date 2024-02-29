5de9a3e54a58f66b44cd827d4779b473ebfffed4
sskotezhov <sskotezhov@edu.hse.ru>
2024-02-29 14:07:35 +0300

    header

--- src/vector2d/vector2d.h
+++ src/vector2d/vector2d.h
@@ -1,26 +1,18 @@
 #pragma once
-#include <iostream>
 
-class vector2d {
+class Vector2d {
 public:
-    vector2d();
-    vector2d(double x, double y);
-    vector2d(long x, long y);
-    vector2d(int x, int y);
-    vector2d operator+(vector2d b);
-    vector2d operator-(vector2d b);
-    vector2d operator-();
-    vector2d operator*(double b);
-    vector2d operator*(long b);
-    vector2d operator*(int b);
-    vector2d operator/(double b);
-    vector2d operator/(long b);
-    vector2d operator/(int b);
-    vector2d round();
-    double lenght_squared();
-    double lenght();
-    vector2d normalized();
-    int manhattan_lenght();
+    Vector2d(double x, double y);
+    const Vector2d operator+(const Vector2d &other);
+    const Vector2d operator-(const Vector2d &other);
+    const Vector2d operator-();
+    const Vector2d operator*(double b);
+    const Vector2d operator/(double b);
+    const Vector2d round();
+    const double length_squared();
+    const double length();
+    const Vector2d normalized();
+    const int manhattan_length();
 
 private:
     double x, y;
