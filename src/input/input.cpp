7767d00f772c1bf0854acc3260e83af79a37f1eb
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-03-01 23:50:30 +0500

    Keyboard singleton implementation

--- src/input/input.cpp
+++ src/input/input.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 #include "input/input.h"
+
+using namespace Input;
+
+InputController &InputController::getInstance() {
+    static InputController singleton;
+    return singleton;
+}
+
+void InputController::setKeyboardEvent(KeyboardEvent event) {
+    this->prev_keyboard_event = current_keyboard_event;
+    this->current_keyboard_event = event;
+}
+
+void InputController::printCurrentEvent() {
+    std::cout << current_keyboard_event.type << " - " << current_keyboard_event.code << std::endl;
+}
--- src/input/input.h
+++ src/input/input.h
@@ -6,4 +6,19 @@ namespace Input {
         uint8_t code;
         uint8_t type;
     };
+
+    class InputController {
+        public:
+            static InputController& getInstance();
+
+            void setKeyboardEvent(KeyboardEvent event);
+            void printCurrentEvent();
+        private:
+            KeyboardEvent current_keyboard_event;
+            KeyboardEvent prev_keyboard_event;
+
+            InputController() {};
+            InputController(const InputController&);  // Prevent copy-construction
+            InputController& operator=(const InputController&);  // Prevent assignment
+    };
 }
