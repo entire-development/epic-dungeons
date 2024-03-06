99a6bf9c8e5428ff7f649298eaf9d67dd4220793
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-03-06 21:54:21 +0300

    create MockDungeonMaker class

---
+++ src/dungeon/makers/MockDungeonMaker.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "dungeon/dungeon.h"
+#include <memory>
+
+namespace dungeon {
+class MockDungeonMaker : IDungeonMaker {
+public:
+    void build() final;
+    std::shared_ptr<Dungeon> getDungeon() final;
+
+private:
+    std::shared_ptr<Dungeon> dungeon;
+};
+}   // namespace dungeon
