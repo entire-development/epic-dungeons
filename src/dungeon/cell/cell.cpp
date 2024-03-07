970d738a05752cca5cd3a49690e2d50ba238665d
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-03-07 15:11:59 +0300

    add connectCells implementation

--- src/dungeon/cell/cell.cpp
+++ src/dungeon/cell/cell.cpp
@@ -4,4 +4,9 @@ namespace dungeon {
 Cell::Cell(dungeon::Position position, bool is_room) : m_position(position), m_is_room(is_room) {}
 
 Room::Room(dungeon::Position position) : Cell(position, true) {}
+
+void connectCells(std::weak_ptr<Cell> cell1, std::weak_ptr<Cell> cell2) {
+    cell1.lock()->m_neighbours.push_back(cell2);
+    cell2.lock()->m_neighbours.push_back(cell1);
+}
 }   // namespace dungeon
