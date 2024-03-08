bceeb38bd907a4d2924b80d08893f8c4aafe830f
sskotezhov <sskotezhov@edu.hse.ru>
2024-03-08 15:04:17 +0300

    fix getSize

--- main.cpp
+++ main.cpp
@@ -47,6 +47,5 @@ int main() {
 
         controller.update();
     }
-
     return 0;
 }
--- src/renderer/sprite.cpp
+++ src/renderer/sprite.cpp
@@ -3,6 +3,9 @@
 namespace graphics {
 Sprite::Sprite(const std::string &path) {
     m_path = cfg::SPRITES_PATH + path;
+    if (!m_texture.loadFromFile(m_path)) {
+        throw std::runtime_error("Error loading sprite from file: " + m_path);
+    }
 }
 
 Sprite &Sprite::setColor(const graphics::Color &color) {
@@ -27,9 +30,6 @@ Sprite &Sprite::setRotation(float rotation_angle) {
 
 sf::Sprite Sprite::toSF() const {
     sf::Sprite sprite;
-    if (!m_texture.loadFromFile(m_path)) {
-        throw std::runtime_error("Error loading sprite from file: " + m_path);
-    }
     sprite.setTexture(m_texture);
     sprite.setColor(m_color.toSF());
     sprite.setRotation(m_angle);
@@ -38,27 +38,24 @@ sf::Sprite Sprite::toSF() const {
 }
 
 Vector2d Sprite::getSize() const {
-    return Vector2d(m_texture.getSize());
+    sf::Vector2u vector = m_texture.getSize();
+    vector.x *= m_x_scale;
+    vector.y *= m_y_scale;
+    return Vector2d(vector);
 }
 
 Sprite &Sprite::toSizeX(const float &x) {
-    float ratio = x / getSize().x();
-    m_x_scale *= ratio;
-    m_y_scale *= ratio;
+    setScale(x / m_texture.getSize().x);
     return *this;
 }
 
 Sprite &Sprite::toSizeY(const float &y) {
-    float ratio = y / getSize().y();
-    m_x_scale *= ratio;
-    m_y_scale *= ratio;
+    setScale(y / m_texture.getSize().y);
     return *this;
 }
 
 Sprite &Sprite::toSize(const float &x, const float &y) {
-    Vector2d tmp = getSize();
-    m_x_scale *= x / tmp.x();
-    m_y_scale *= y / tmp.y();
+    setScale(x / m_texture.getSize().x, y / m_texture.getSize().y);
     return *this;
 }
 }   // namespace graphics
