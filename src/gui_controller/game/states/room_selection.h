3141a8dea3f8ff65e8ec9504c8e5f504e7074e8b
Nikolay Khadzakos <hadzakos@gmail.com>
2024-03-08 18:46:24 +0300

    render as shared_ptr

--- src/gui_controller/game/states/room_selection.h
+++ src/gui_controller/game/states/room_selection.h
@@ -11,12 +11,12 @@ class RoomSelection : public GameState {
     virtual void enter(GameMachine *gm) {
         std::shared_ptr<dungeon::Dungeon> d = gm->m_engine.lock().get()->getDungeon();
         neighbours = d->getRoomNeighbours(std::dynamic_pointer_cast<dungeon::Room>(d->getCurrentCell().lock()));
-        graphics::Renderer *r = gm->m_renderer.lock().get();
+        std::shared_ptr<graphics::Renderer> r = gm->m_renderer.lock();
         render(r, d);
     }
 
     virtual void update(GameMachine *gm) {
-        graphics::Renderer *r = gm->m_renderer.lock().get();
+        std::shared_ptr<graphics::Renderer> r = gm->m_renderer.lock();
         std::shared_ptr<dungeon::Dungeon> d = gm->m_engine.lock().get()->getDungeon();
         bool pressed_right = keyboard::isPressed(keyboard::KEY_RIGHT) || keyboard::isPressed(keyboard::KEY_D);
         bool pressed_left = keyboard::isPressed(keyboard::KEY_LEFT) || keyboard::isPressed(keyboard::KEY_LEFT);
@@ -31,17 +31,17 @@ class RoomSelection : public GameState {
         if (pressed_right) {
             r_selected = (r_selected + 1) % neighbours.size();
             is_key_pressed = true;
-            render(r, d, neighbours[r_selected].lock());
+            render(r, d);
         } else if (pressed_left) {
             r_selected = (r_selected - 1 + neighbours.size()) % neighbours.size();
             is_key_pressed = true;
-            render(r, d, neighbours[r_selected].lock());
+            render(r, d);
         } else if (pressed_enter) {
             // next state
         }
     }
 
-    void render(graphics::Renderer *r, std::shared_ptr<dungeon::Dungeon> d, std::shared_ptr<dungeon::Cell> selected_room = nullptr) {
+    void render(std::shared_ptr<graphics::Renderer> r, std::shared_ptr<dungeon::Dungeon> d) {
         r->clear();
         r->drawText(50, 50, "Select a room");
         std::vector<std::shared_ptr<dungeon::Cell>> cells = d->getCells();
@@ -57,7 +57,7 @@ class RoomSelection : public GameState {
                 w = cfg::CELL_SIZE * 3;
                 color = "#cc2e0e";
             }
-            if (cell == selected_room)
+            if (cell == neighbours[r_selected].lock())
                 stroke_color = "#ffffff";
             if (cell == d->getCurrentCell().lock())
                 stroke_color = "#140ecc";
