db06e4ed36f519aa7a1acc93433916eb877bddc6
sskotezhov <160405460+sskotezhov@users.noreply.github.com>
2024-03-20 16:53:28 +0300

    Update pick_and_knives.h

--- src/engine/weapons/pick_and_knives.h
+++ src/engine/weapons/pick_and_knives.h
@@ -6,6 +6,7 @@ namespace engine {
 namespace items {
 
 struct PickAndKnives : public Weapon {
+    Pistol() : Weapon() {
     id = "pick_and_knives";
     name = "Pick and Knives";
     attackMod = 0;
@@ -13,6 +14,7 @@ struct PickAndKnives : public Weapon {
     maxDamage = 8;
     criticalChanceMod = 6;
     speedMod = 8;
+    }
 };
 
 }   // namespace items
